35553a127fbd4b2ccfed587fa3ed7ded44eb038f
saki <saki@yee.com>
2021-08-01 18:59:59 +0800

    hooks

--- src/redux/container/count.js
+++ src/redux/container/count.js
@@ -1,6 +1,36 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
 import {increment, decrement, incrementAsync} from "../action/count";
+
+function CountFunc() {
+    const [num, setNum] = React.useState(20)
+    const [name, setName] = React.useState('ttttt')
+    const inRef = React.useRef()
+    React.useEffect(
+        () => {
+            // mounted
+            console.log('watch', num, name);
+            return () => {
+                // 卸载前执行 todo
+                console.log('destroy', num);
+            }
+        }, [num, name]
+    )
+    function addIt() {
+        setNum(num => {
+            console.log('setNum', num);
+            return num + 2
+        })
+        setName(name => name + 1)
+    }
+    return (
+        <Fragment key={Math.random()}>
+            <h2>hooks测试----{num}----{name}</h2>
+            <button onClick={addIt}>+1s</button>
+            <input onInput={() => {console.log(inRef.current.value)}} ref={inRef} type="text"/>
+        </Fragment>
+    )
+}
 
 class Count extends Component {
     numRef = React.createRef()
@@ -23,7 +53,7 @@ class Count extends Component {
     }
     render() {
         return (
-            <div>
+            <Fragment>
                 <h2>Count组件求和{this.props.count}，下方组件总人数为{this.props.personCount}</h2>
                 <select ref={this.numRef}>
                     <option value="1">1</option>
@@ -34,7 +64,8 @@ class Count extends Component {
                 <button onClick={this.decrement}>-</button>
                 <button onClick={this.incrementOdd}>奇数时加</button>
                 <button onClick={this.incrementAsync}>异步加</button>
-            </div>
+                <CountFunc/>
+            </Fragment>
         );
     }
 }
--- src/redux/container/person.js
+++ src/redux/container/person.js
@@ -3,6 +3,10 @@ import {connect} from "react-redux";
 import {newPerson} from "../action/person";
 
 class Person extends Component {
+    state = {
+        name: 'aaa',
+        age: 14
+    }
     addPerson = () => {
         const obj = {
             name: this.newName.value,
@@ -10,10 +14,28 @@ class Person extends Component {
         }
         this.props.newPerson(obj)
     }
+    setStateObj = () => {
+        this.setState({
+            name: 'bbbb'
+        }, () => {
+            // callback在render后执行
+            console.log(this.state);
+        })
+    }
+    setStateFunc = () => {
+        this.setState((state, props) => {
+            console.log(state, props);
+            return {name: state.name + props.count}
+        }, () => {
+            // callback在render后执行
+            console.log(this.state);
+        })
+    }
     render() {
         return (
             <div>
                 <h3>上方组件和{this.props.count}</h3>
+                <h4>{this.state.name}</h4>
                 <input ref={c => this.newName = c} type="text"/>
                 <input ref={c => this.newAge = c} type="text"/>
                 <button onClick={this.addPerson}>addNewPerson</button>
@@ -22,6 +44,8 @@ class Person extends Component {
                         return <div key={Math.random()}>{item.name}---{item.age}</div>
                     })
                 }
+                <button onClick={this.setStateObj}>setStateObj</button>
+                <button onClick={this.setStateFunc}>setStateFunc</button>
             </div>
         );
     }
