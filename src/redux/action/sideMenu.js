88bdb32a0e8ee4451845de12cacb1d04d0a9da1e
electus <uasic@outlook.com>
2022-06-21 23:52:16 +0800

    feat: opt

--- src/redux/action/sideMenu.js
+++ src/redux/action/sideMenu.js
@@ -1,19 +1,15 @@
 import { SET_HEADER_TITLE } from '../constant';
 
-export const setHeaderTitle = title => {
-  return dispatch => {
-    dispatch({
-      type: SET_HEADER_TITLE,
-      data: title
-    });
-  };
+export const setHeaderTitle = title => dispatch => {
+  dispatch({
+    type: SET_HEADER_TITLE,
+    data: title
+  });
 };
 
-export const refreshSideMenu = data => { 
-  return dispatch => {
-    dispatch({
-      type: 'refresh',
-      data: data
-    });
-  }
-}
+export const refreshSideMenu = data => dispatch => {
+  dispatch({
+    type: 'refresh',
+    data: data
+  });
+};
